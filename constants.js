250308987ee9d5f00016be60358c008a20bbd20f
Marc <81155509+Marc-Industries@users.noreply.github.com>
2025-12-05 14:13:19 +0100

    Update shop name in constants.js

--- constants.js
+++ constants.js
@@ -7,7 +7,7 @@ export const TELEGRAM_CHAT_ID = '929092833';
 export const GRAMS_PER_PERSON = 250; // Valore di default modificabile
 
 // Info Negozio
-export const SHOP_NAME = "Market di Marcon Francesco";
+export const SHOP_NAME = "Belmarket di Marcon Francesco";
 export const SHOP_ADDRESS = "Via Cantoni di Sotto, 60, 31037 Bessica di Loria TV";
 export const MAPS_LINK = "https://maps.app.goo.gl/VzZFGJXa4bUE3DBn8";
 export const PHONE_NUMBER = "347 111 5927";
